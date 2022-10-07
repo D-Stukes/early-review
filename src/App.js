0d5e3601d6258fc3ba825ae9eef8b387238dbdd0
D-Stukes <dstukes888@gmail.com>
2022-10-07 13:00:06 -0400

    updated review to use ternary for button click event

--- src/App.js
+++ src/App.js
@@ -2,10 +2,14 @@ import './App.css';
 import { useState } from 'react';
 
 function App() {
+  //useState takes initial value for variable as argument
+  //In the array, we have TWO values:
+  //first value: the piece of stte
+  //second value: the handler function to update
 
     // const [] = useState('light')
 
-  
+  //destructured
     const[mode, setMode] = useState('light')
  
     const changeMode = () => {
@@ -16,13 +20,19 @@ function App() {
       }
     }
 
+    //alternative
+    // mode === 'light' ? setMode('dark') : setMode('light')
+
     console.log(mode)
+    
     return(
       <div className={mode}>
 
-          <button onClick={changeMode}>    
+          {/* <button onClick={changeMode}>    
             Change to dark mode
-          </button>
+          </button> */}
+
+<button onClick={changeMode}> Change to {mode === 'light' ? 'dark': 'light'} mode </button>
           <h1>{mode}</h1>
 
       </div>
